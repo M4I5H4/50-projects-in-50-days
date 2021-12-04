79d2eae8dcf41c7a6b188759e75c1742f727f490
M4I5HA <maisha.adamsam@gmail.com>
2021-12-04 17:09:26 +0000

    day 7 task complete

--- 6_Scroll_Animation/script.js
+++ 6_Scroll_Animation/script.js
@@ -1,3 +1,5 @@
+// import { dates } from "./data/data.js";
+
 const boxes = document.querySelectorAll(".box");
 
 window.addEventListener("scroll", checkBoxes);
@@ -12,10 +14,19 @@ function checkBoxes() {
     const boxTop =
       box.getBoundingClientRect().top;
 
-      if (boxTop < triggerBottom) {
-          box.classList.add('show')
-      } else {
-          box.classList.remove('show')
-      }
+    if (boxTop < triggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
   });
 }
+
+// dates.forEach((subject, index) => {
+//   const card = `<div class="box"><h2 class="box-title">${subject.title}</h2>
+//     <span class="box-date">${subject.date}</span>
+//     <p class="box-summary">${subject.summary}</p></p><button class="timeline-item-more-info" id="${index}">Click for more information!</button></div>`;
+//   const element = document.createElement("div");
+//   element.innerHTML = card;
+//   document.body.appendChild(element);
+// });
